001335495e4ba3d123a4f7851afd67099b7f98f1
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-06-27 05:19:55 +0300

    Create a posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "posts/new"
+  get "posts/create"
+  get "posts/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
