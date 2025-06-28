9ef5e1fed5ee9a58cbb478843e919a59c7ff6492
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-06-28 11:51:43 +0300

    Add create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,16 @@ class PostsController < ApplicationController
   before_action :authenticate_member!, only: [ :new, :create ]
 
   def new
-    @post = Post.new
+    @post = current_member.posts.new
   end
 
   def create
+    @post = current_member.posts.new
+    if @post.save
+      redirect_to index_path
+    else
+      render "new"
+    end
   end
 
   def index
