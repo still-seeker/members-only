b73a466752ae41c44fa8f8f43880d5075143821d
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-07-02 01:19:06 +0300

    Add foreign key reference to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_27_151709) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_01_220232) do
   create_table "members", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -28,7 +28,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_27_151709) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "author_id"
+    t.integer "member_id", null: false
+    t.index ["member_id"], name: "index_posts_on_member_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +43,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_27_151709) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "posts", "members"
 end
