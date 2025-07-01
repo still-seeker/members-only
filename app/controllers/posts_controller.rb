70d96040f2e96cc234af36f08a73add8c36e6c76
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-07-01 14:37:27 +0300

    Add author_id to params hash expectations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,6 @@ class PostsController < ApplicationController
 
   private
     def post_params
-      params.expect(post: [ :title, :content ])
+      params.expect(post: [ :title, :content, :author_id ])
     end
 end
