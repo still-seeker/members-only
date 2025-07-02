71fe3152fec14e371a8226c7b1481270c6d8f57d
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-07-02 11:03:45 +0300

    Edit routes to destroy member session with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :members
+  devise_for :members do
+    get "/members/sign_out" => "devise/sessions#destroy"
+  end
   get "up" => "rails/health#show", as: :rails_health_check
+
 
   resources :posts
   # Defines the root path route ("/")
