3ddcdfff744b68fe141bb828cf57b6e46613518a
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-07-02 18:35:05 +0300

    Update validations for the email column in the table

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,4 +5,5 @@ class Member < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts
+  validates :email, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
 
-  resources :posts, :only[:new, :create, :index]
+  resources :posts
   # Defines the root path route ("/")
   root to: "posts#index"
 end
